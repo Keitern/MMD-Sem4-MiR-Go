6e37e4f5585c896113d26f7f13d2051214be46f0
valde <114578370+HerbForest@users.noreply.github.com>
2025-05-28 12:25:32 +0200

    added json library
    json library

--- MiR Go Solution/src/components/CasesModul.vue
+++ MiR Go Solution/src/components/CasesModul.vue
@@ -1,8 +1,27 @@
-<script>
+<script setup>
+import { useStoryblokApi } from '@storyblok/vue';
+import { ref, onMounted } from 'vue';
 
+const storyblokApi = useStoryblokApi();
+const stories = ref([]);
+
+onMounted(async () => {
+  const response = await storyblokApi.get('cdn/stories', {
+    version: 'published',
+    starts_with: 'products/mir-go',
+    per_page: 25,
+    page: 1,
+    resolve_links: 'url',
+    sort_by: 'published_at:desc',
+    language: 'en',
+  });
+  stories.value = response.data.stories;
+});
 </script>
-<template>
 
+<template>
+  <div v-for="story in stories" :key="story.id">
+    <h2>{{ story.name }}</h2>
+    <!-- Vis yderligere data efter behov -->
+  </div>
 </template>
-<style>
-</style>
